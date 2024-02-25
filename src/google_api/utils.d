5f6e8f4b09129d53c15da3a699eea81a99ba0c69
Nickolay Bukreyev <buknik95@yandex.ru>
2024-02-26 04:38:44 +0700

    Add `concat` helper

--- src/google_api/utils.d
+++ src/google_api/utils.d
@@ -10,3 +10,62 @@ inout(char)[ ] validateUtf(return scope inout(ubyte)[ ] data) pure {
     result.validate();
     return result;
 }
+
+///
+char[ ] concat(Args...)(scope const Args args) nothrow pure {
+    import std.array: uninitializedArray;
+    import std.conv: toChars;
+
+    enum extra = {
+        size_t n;
+        static foreach (T; Args)
+            static if (is(T == char))
+                n++;
+            else static if (is(T == ubyte))
+                n += 3;
+            else static if (is(T == byte))
+                n += 4;
+            else static if (is(T == ushort) || is(T == bool))
+                n += 5;
+            else static if (is(T == short))
+                n += 6;
+            else static if (is(T == uint))
+                n += 10;
+            else static if (is(T == int))
+                n += 11;
+            else static if (is(T == ulong) || is(T == long))
+                n += 20;
+            else
+                static assert(is(immutable T == immutable char[ ]), "Invalid type `", T, "`");
+        return n;
+    }();
+    size_t i = extra;
+    static foreach (j, T; Args)
+        static if (is(immutable T == immutable char[ ]))
+            i += args[j].length;
+
+    auto result = uninitializedArray!(char[ ])(i);
+    i = 0;
+    static foreach (arg; args)
+        static if (is(immutable typeof(arg) == immutable char[ ]))
+            result[i .. i += arg.length] = arg;
+        else static if (is(typeof(arg) == const char))
+            result[i++] = arg;
+        else static if (is(typeof(arg) == const bool))
+            if (arg)
+                result[i .. i += 4] = "true";
+            else
+                result[i .. i += 5] = "false";
+        else {{
+            static if (arg.sizeof < 4) {
+                static if (__traits(isUnsigned, arg))
+                    uint arg = arg;
+                else
+                    int arg = arg;
+            }
+            foreach (c; arg.toChars)
+                result[i++] = c;
+        }}
+
+    return result[0 .. i];
+}
--- test/google_api/test.d
+++ test/google_api/test.d
@@ -5,9 +5,7 @@ import google_api.utils;
 
 @safe:
 
-//
-// google_api.utils:
-//
+// google_api.utils.validateUtf:
 
 pure unittest {
     import std.exception: assertThrown;
@@ -18,4 +16,29 @@ pure unittest {
     assert(validateUtf(null) is null);
 
     assertThrown!UTFException(validateUtf([0x80]));
+}
+
+// google_api.utils.concat:
+
+nothrow pure unittest {
+    immutable b = true;
+    char[2] sep = ": ";
+    string s =
+        concat("test", ' ', b, sep[ ], ubyte(0), byte(1), ushort(2), short(3), 4u, 5, 6uL, 7L);
+    assert(s == "test true: 01234567");
+
+    assert(concat() == "");
+    assert(concat(false) == "false");
+    assert(concat(ubyte.max) == "255");
+    assert(concat(ushort.max) == "65535");
+    assert(concat(uint.max) == "4294967295");
+    assert(concat(ulong.max) == "18446744073709551615");
+    assert(concat(byte.min) == "-128");
+    assert(concat(short.min) == "-32768");
+    assert(concat(int.min) == "-2147483648");
+    assert(concat(long.min) == "-9223372036854775808");
+
+    static assert(!__traits(compiles, concat(2.5)));
+    static assert(!__traits(compiles, concat(null)));
+    static assert(!__traits(compiles, concat((char*).init)));
 }
